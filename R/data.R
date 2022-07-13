d941286c590c3165d4290b07b43b44945dfa1def
benRenard <benjamin.renard@inrae.fr>
2022-07-13 16:43:54 +0200

    Added new dataset: IPCC SREX regions

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' IPCC SREX regions
+#'
+#' Boundaries of the SREX regions used in various IPCC reports
+#'
+#' @source \url{https://www.ipcc-data.org/guidelines/pages/ar5_regions.html}
+"SREXregions"
